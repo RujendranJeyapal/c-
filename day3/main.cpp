0c385c3f9eef15601691fb8e32430ad173e2a039
RujendranJeyapal <95875459+RujendranJeyapal@users.noreply.github.com>
2022-06-17 19:16:01 +0530

    Add files via upload

---
+++ day3/main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+
+using std::cin;
+using std::cout;
+
+int main()
+{
+    int row,col;
+    cout<<"Enter rows\n";
+    cin>>row;
+    cout<<"\nEnter columns\n";
+    cin>>col;
+
+     int *matrix=new int[row*col]; 
+
+      for( int i=0;i<row;i++ )
+      {
+             for( int j=0;j<col;j++ )
+             {
+                
+             }
+
+      }
+
+
+}
+

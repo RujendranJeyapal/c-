b20ad237882668de06e2c18b7d9d4e840da67131
RujendranJeyapal <95875459+RujendranJeyapal@users.noreply.github.com>
2022-06-19 17:52:23 +0530

    Add files via upload

--- day3/main.cpp
+++ day3/main.cpp
@@ -1,28 +1,54 @@
-#include<iostream>
 
 
+#include "LeaderFind.h"
 using std::cin;
 using std::cout;
 
+
 int main()
 {
-    int row,col;
-    cout<<"Enter rows\n";
-    cin>>row;
-    cout<<"\nEnter columns\n";
-    cin>>col;
+    
+          int row,col;
+          cout<<"Enter row\n";
+          cin>>row;
 
-     int *matrix=new int[row*col]; 
+         cout<<"\nEnter colums\n";
+         cin>>col;
 
-      for( int i=0;i<row;i++ )
-      {
-             for( int j=0;j<col;j++ )
-             {
-                
-             }
+         int **city=(int**)malloc(row*sizeof(int*));
+ 
+         for(int i=0; i<row; i++)
+         {
+             city[i]=(int*)malloc(col*sizeof(int));  
+         }
 
-      }
+         for( int i=0;i<row;i++ )
+         {
+              for( int j=0;j<col;j++ )
+              {
+                  cin>>city[i][j];
+              }
+        }
 
+        int *group=(int*)malloc((row*col)*sizeof(int));
+
+        LeaderFind callFindLeader;
+
+        callFindLeader.getGroup(city,group,row,col);
+
+        cout<<"Groups are:";
+
+        for( int i=0;i<(row*col); i++ )
+        {
+              if( group[i]!=0 )
+              {
+                  cout<<group[i]<<" ";
+              }
+        }
+
+         cout<<"\nThe Leader of the group is: "<<callFindLeader.findLeader(group)<<"\n";
+
+         free(city);
+         free(group);
 
 }
-
